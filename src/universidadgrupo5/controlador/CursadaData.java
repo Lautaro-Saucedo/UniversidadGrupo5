6c0ecfd06d8a2c253c73a28a1c0866acc7f792eb
Leandro Xavier Weber <80133255+LXWeber@users.noreply.github.com>
2021-05-14 12:34:50 -0300

    CursadaData

--- src/universidadgrupo5/controlador/CursadaData.java
+++ src/universidadgrupo5/controlador/CursadaData.java
@@ -3,8 +3,11 @@ package universidadgrupo5.controlador;
 
 import java.sql.*;
 import java.util.*;
+import javax.swing.JOptionPane;
+import universidadgrupo5.modelo.Alumno;
 import universidadgrupo5.modelo.Conexion;
 import universidadgrupo5.modelo.Cursada;
+import universidadgrupo5.modelo.Materia;
 
 /**
  *
@@ -22,13 +25,127 @@ public class CursadaData {
         
         try{
             PreparedStatement ps = conexion.prepareStatement(query);
-            ps.setInt(1,cursada.getAlumno().getId_alumno());
-            ps.setInt(2,cursada.getMateria().getId_materia());
+            ps.setInt(1,cursada.getId_alumno().getId_alumno());
+            ps.setInt(2,cursada.getId_materia().getId_materia());
             ps.setDouble(3,cursada.getNota());
             ps.execute();
             ps.close();
         }catch(SQLException ex){
-            
+            JOptionPane.showInputDialog(null,"No se pudo inscribir");
         }
+    }
+    
+    public List<Cursada> obtenerCursadas(){
+        ArrayList<Cursada> lista = new ArrayList<>();
+        String query = "SELECT * FROM cursada";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Cursada aux= new Cursada();
+                Alumno alu = new Alumno();
+                Materia mat = new Materia();
+                aux.setId_cursada(rs.getInt("id_cursada"));
+                alu.setId_alumno(rs.getInt("id_alumno"));
+                aux.setId_alumno(alu);
+                mat.setId_materia(rs.getInt("id_materia"));
+                aux.setId_materia(mat);
+                aux.setNota(rs.getInt("nota"));
+                lista.add(aux);
+            }
+            ps.close();
+        } catch (SQLException sqle){
+            JOptionPane.showMessageDialog(null, "No se pudo ostener cursadas");
+        }
+        return lista;
+    }
+    
+    public List<Cursada> obtenerCursadasXAlumno(int id_alumno){
+        ArrayList<Cursada> lista = new ArrayList<>();
+        String query="SELECT * FROM cursada WHERE id_alumno=?";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, id_alumno);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Alumno alu = new Alumno();
+                Materia mat = new Materia();
+                Cursada aux= new Cursada();
+                aux.setId_cursada(rs.getInt("id_cursada"));
+                alu.setId_alumno(rs.getInt("id_alumno"));
+                aux.setId_alumno(alu);
+                mat.setId_materia(rs.getInt("id_materia"));
+                aux.setId_materia(mat);
+                aux.setNota(rs.getInt("nota"));
+                lista.add(aux);
+            }
+            ps.close();
+        } catch (SQLException sqle){
+            JOptionPane.showMessageDialog(null, "No se pudo obtener cursadas de ese alumno");
+        }
+        return lista;
+    }
+    
+    public List<Materia> obtenerMateriasCursadas(int id_alumno){
+        ArrayList<Materia> lista = new ArrayList<>();
+        String query = "SELECT id_materia FROM cursada WHERE id_alumno=?";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, id_alumno);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Materia aux= new Materia();
+                aux.setId_materia(rs.getInt("id_materia"));
+                lista.add(aux);
+            }
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "No se pudo obtener materias de ese alumno");
+        }
+        return lista;
+    }
+    
+    public List<Materia> obtenerMateriasNOCursadas(int id_alumno){
+        ArrayList<Materia> lista = new ArrayList<>();
+        String query = "SELECT id_materia FROM cursada WHERE NOT id_alumno=?";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, id_alumno);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Materia aux= new Materia();
+                aux.setId_materia(rs.getInt("id_materia"));
+                lista.add(aux);
+            }
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "No se pudo obtener materias de ese alumno");
+        }
+        return lista;
+    }
+    
+    public void borrarCursadaDeUnaMateriaDeUnAlumno(int id_alumno , int id_materia){
+        String query = "DELETE cursada FROM cursada WHERE id_alumno=? , id_materia=?";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(id_alumno, id_alumno);
+            ps.setInt(id_materia, id_materia);
+            ps.execute();
+            ps.close();
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "No se pudo eliminar");
+        }
+    }
+    
+    public void actualizarNotaCursada(int id_Materia, int id_alumno, int nota){
+        String query="UPDATE cursada SET nota=? WHERE id_materia=?, id_alumno=?";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(nota,nota);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException sqle){
+            JOptionPane.showMessageDialog(null, "No se pudo actualizar");
+        } 
     }
 }
--- src/universidadgrupo5/modelo/Cursada.java
+++ src/universidadgrupo5/modelo/Cursada.java
@@ -2,23 +2,23 @@ package universidadgrupo5.modelo;
 
 public class Cursada {
     private int id_cursada;
-    private Alumno alumno;
-    private Materia materia;
+    private Alumno id_alumno;
+    private Materia id_materia;
     private double nota;
 
     public Cursada() {
     }
 
-    public Cursada(int id_cursada, Alumno alumno, Materia materia, double nota) {
+    public Cursada(int id_cursada, Alumno id_alumno, Materia id_materia, double nota) {
         this.id_cursada = id_cursada;
-        this.alumno = alumno;
-        this.materia = materia;
+        this.id_alumno = id_alumno;
+        this.id_materia = id_materia;
         this.nota = nota;
     }
 
-    public Cursada(Alumno alumno, Materia materia, double nota) {
-        this.alumno = alumno;
-        this.materia = materia;
+    public Cursada(Alumno id_alumno, Materia id_materia, double nota) {
+        this.id_alumno = id_alumno;
+        this.id_materia = id_materia;
         this.nota = nota;
     }
 
@@ -30,20 +30,20 @@ public class Cursada {
         this.id_cursada = id_cursada;
     }
 
-    public Alumno getAlumno() {
-        return alumno;
+    public Alumno getId_alumno() {
+        return id_alumno;
     }
 
-    public void setAlumno(Alumno alumno) {
-        this.alumno = alumno;
+    public void setId_alumno(Alumno id_alumno) {
+        this.id_alumno = id_alumno;
     }
 
-    public Materia getMateria() {
-        return materia;
+    public Materia getId_materia() {
+        return id_materia;
     }
 
-    public void setMateria(Materia materia) {
-        this.materia = materia;
+    public void setId_materia(Materia id_materia) {
+        this.id_materia = id_materia;
     }
 
     public double getNota() {
